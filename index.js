94366ff3eb1f329bda0dcb560a39e3cd3778c401
Ankit singh <singhankit0460@gmail.com>
2024-05-03 00:20:44 +0530

    final update search bar working

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const apiKey = "2381c3c9c7dc4daea5110196517dbab5"
 
 const blogContainer = document.getElementById("blog-container");
 
+const searchField = document.getElementById("search-input");
+const searchButton = document.getElementById("search-button");
+
+
 async function fetchRandomNews() {
   try {
-    const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&pageSize=10&apiKey=${apiKey}`;
+    const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&pageSize=12&apiKey=${apiKey}`;
     const response = await fetch(apiUrl);
     const data = await response.json();
     return data.articles;
@@ -14,6 +18,34 @@ async function fetchRandomNews() {
     return [];
   }
 }
+
+searchButton.addEventListener("click", async () => {
+  const query = searchField.value.trim();
+  if (query !== "") {
+    try {
+      const articles = await fetchNewsQuery(query)
+      displayBlogs(articles);
+    } catch (error) {
+      console.log("Error fetching news by Query", error);
+    }
+  }
+});
+
+async function fetchNewsQuery(query) {
+  try {
+    const apiUrl = `https://newsapi.org/v2/everything?q=${query}&pageSize=12&apiKey=${apiKey}`;
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    return data.articles;
+
+  } catch (error) {
+    console.error("Error Fetching random news", error);
+    return [];
+  }
+}
+
+
+
 function displayBlogs(articles) {
   blogContainer.innerHTML = "";
   articles.forEach((article) => {
@@ -24,13 +56,21 @@ function displayBlogs(articles) {
     img.src = article.urlToImage;
     img.alt = article.title;
     const title = document.createElement("h2");
-    title.textContent = article.title;
+    const Truncated = article.title.length > 30 ?
+      article.title.slice(0, 30) + "...." :
+      article.title;
+    title.textContent = Truncated;
     const description = document.createElement("p")
     description.textContent = article.description;
 
     blogCart.appendChild(img);
     blogCart.appendChild(title);
     blogCart.appendChild(description);
+    blogCart.addEventListener("click", () => {
+      window.open(article.url, "_blank");
+
+
+    })
     blogContainer.appendChild(blogCart);
 
   })
